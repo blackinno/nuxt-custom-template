c4b45946b37ec4a7bdb83864583ff62063574eaf
blackinno <ruthamnong.thotsaphon@gmail.com>
2021-08-02 13:45:16 +0700

    set demo for storybook

---
+++ components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: { type: String, reuired: true, default: '' },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ stories/Button.stories.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'Button',
+}
+
+export const DefautButton = () => '<Button text="something"/>'
+export const ButtonWithPrimary = () => '<Button text="Primary"/>'
